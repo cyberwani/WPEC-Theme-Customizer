eeb5ca2b6b306cab6eddde5131f889679456d583
Jack Mahoney <jackmahoney212@gmail.com>
2012-06-02 16:22:43 +1000

    added section class and specific controls ready for templating

---
+++ wpec_theme_customizer_section.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Section class for containing controls, extends Radagast
+ */
+class WPEC_Theme_Customizer_Section extends Radagast_The_Brown{
+	
+	public function __construct($gandalf, $name, $id){
+		parent::__construct($gandalf, $id);
+	}
+}
+
+?>
